a62c854815339af5bc60aea069a07e449afad2ce
Martin Svoren <msvoro@gmail.com>
2021-07-30 10:53:54 +0200

    WIP filter avoid decimal integer

--- src/components/Customers/operations/filter.ts
+++ src/components/Customers/operations/filter.ts
@@ -14,112 +14,31 @@ export const filterDataGrid = (filter: ObjectType, onFilterCustomers: (event: Ob
         // bigInteger, seq bude, zoznam v [bigInteger, seq] ktory dam do for cyklu pomoze mi Object.keys(filter).length z toho vytovrim for to bude lenght
         const columnFieldList = Object.keys(filter)
         console.log(columnFieldList,'columnFieldList'); 
+
+        const  integerColumns = ['seq', 'bigInteger']
         
         for (let i = 0; i < columnFieldList.length; i++){
-            // console.log(columnFieldList.length,'columnFieldListcolumnFieldList'); 
-            // console.log(columnFieldList[i],'columnFieldList[i]'); 
-            console.log(filterDuplicate,'filterDuplicate'); 
-            console.log(columnFieldList[0],'columnFieldList[0]'); 
-            filterDuplicate[columnFieldList[i]] = { _eq:  parseInt(filterDuplicate[columnFieldList[i]]) }
-            // filterDuplicate.seq = { _eq:  filterDuplicate.seq }
-            console.log(filterDuplicate,'filterDuplicate'); 
+            console.log(columnFieldList[i],'columnFieldList[i]'); 
+            
+            if(integerColumns.includes(columnFieldList[i])){
+                let filterNumberValue = null
+                if(filterDuplicate[columnFieldList[i]]){
+                    filterNumberValue = filterDuplicate[columnFieldList[i]].replace(/[^0-9]/g, '').slice(0,19)
+                    if(filterNumberValue === ""){
+                        filterNumberValue = null
+                    }
+                }
+
+                console.log(filterNumberValue,'filterNumberValue'); 
+                filter[columnFieldList[i]] = filterNumberValue
+                filterDuplicate[columnFieldList[i]] = { _eq:  filterNumberValue }
+            }
         }   
-
-        // filterDuplicate.bigInteger = { _eq:  filterDuplicate.bigInteger }
-        // filterDuplicate.seq = { _eq:  filterDuplicate.seq }
-
+        
+        console.log(filterDuplicate,'filterDuplicate'); 
         console.log(filter,'filterrrr'); 
-
-        // const filterColumnField = Object.keys(filter)[0]
-        // const filterColumnField2 = Object.keys(filter)[1]
-        // const filterColumnField = Object.keys(filter)[i]
-
-        // console.log(filterColumnField,'filterColumnField'); 
     
-        // // let filterValue = filter[filterColumnField]
-        // // let filterValue2 = filter[filterColumnField2]
-        // let filterValue = filter[filterColumnField[i]]
-
-        // console.log(filterValue,'filterValue'); 
-    
-        // // const filteredQuery = getFilteredQuery(filter, filterValue, filterColumnField)
-        // console.log(Object.keys(filter),'Object.keys(filter)x'); 
-        
-        // // const filteredQueryForGraphQl: ObjectType = {}
-        // // const filteredQueryForGraphQl2: ObjectType = {}
-        // const filteredQueryForGraphQl: ObjectType = {}
-        // const filteredQueryForGraphQl2: ObjectType = {}
-    
-        // // if(filterValue2 === ""){
-        // //     filterValue2 = null
-        // // }
-        // if(filterValue === ""){
-        //     filterValue = null
-        // }
-        
-        // // filteredQueryForGraphQl[filterColumnField] = { _eq:  filterValue }
-        // // filteredQueryForGraphQl2[filterColumnField2] = { _eq: filterValue2}
-
-        // filteredQueryForGraphQl[filterColumnField] = { _eq:  filterValue }
-        // // filteredQueryForGraphQl2[filterColumnField2] = { _eq: filterValue2}
-    
-        // const connect = Object.assign(filteredQueryForGraphQl, filteredQueryForGraphQl2)
-
-        // console.log(connect,'connect'); 
-    
-        // // console.log(Object.keys(filter).length,'Object.keys(filter).length'); 
-        // // if(Object.keys(filter).length > 1){
-        // //     alert("adfqwer")
-        // // }
-    
-        // // console.log(filterColumnField,'filterColumnField15'); 
-    
-        // // console.log(filterColumnField, 'filterColumnField');
-        // // console.log(filterValue, 'filterValue');
-    
-        // //funguje s jednym query
-        // // onFilterCustomers(filteredQuery)
         onFilterCustomers(filterDuplicate)
-    // }   
-    // const filterColumnField = Object.keys(filter)[0]
-    // const filterColumnField2 = Object.keys(filter)[1]
-
-    // let filterValue = filter[filterColumnField]
-    // let filterValue2 = filter[filterColumnField2]
-
-    // const filteredQuery = getFilteredQuery(filter, filterValue, filterColumnField)
-    // console.log(Object.keys(filter),'Object.keys(filter)x'); 
-    
-    // const filteredQueryForGraphQl: ObjectType = {}
-    // const filteredQueryForGraphQl2: ObjectType = {}
-
-    // if(filterValue2 === ""){
-    //     filterValue2 = null
-    // }
-    // if(filterValue === ""){
-    //     filterValue = null
-    // }
-    
-    // filteredQueryForGraphQl[filterColumnField] = { _eq:  filterValue }
-    // filteredQueryForGraphQl2[filterColumnField2] = { _eq: filterValue2}
-
-    // const connect = Object.assign(filteredQueryForGraphQl, filteredQueryForGraphQl2)
-    // console.log(connect,'connect'); 
-
-    // console.log(Object.keys(filter).length,'Object.keys(filter).length'); 
-    // // if(Object.keys(filter).length > 1){
-    // //     alert("adfqwer")
-    // // }
-
-    // console.log(filterColumnField,'filterColumnField15'); 
-
-    // console.log(filterColumnField, 'filterColumnField');
-    // console.log(filterValue, 'filterValue');
-
-    // //funguje s jednym query
-    // // onFilterCustomers(filteredQuery)
-    // onFilterCustomers(connect)
-    // setCurrentPageToOne(onChangePageCustomers)
 }
 
 // const getFilteredQuery = (filter:ObjectType, filterValue: string, filterColumnField: string) => {
