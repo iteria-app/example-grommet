f2c785b318b74035715cb9166493d6cd1f572076
Martin Svoren <msvoro@gmail.com>
2021-08-02 16:39:58 +0200

    add console.log

--- src/components/test/GeneratedUtilization.tsx
+++ src/components/test/GeneratedUtilization.tsx
@@ -8,6 +8,7 @@ export const GeneratedUtilization: React.FC = () => {
   const [result] = useAllUtilizationQuery()
   const { data, error, fetching } = result;
   if (error) return <p>Oh no... {error.message}</p>;
+  console.log(data)
   return (
     <Box>
       <h1>Generated utilization</h1>
